f9614bb88f7f0bff492df2959eb0995c4a1762dc
Stalk Spectrum <stalkspectrum@wildfielded.site>
2023-08-09 21:55:00 +0800

    Workday save4 09-08-2023

--- main_bottle.py
+++ main_bottle.py
@@ -7,6 +7,15 @@ ROOT_INDEX = 'adds_srv/index.html'
 
 ''' =====----- Server resources -----===== '''
 
+def server_root() -> str:
+    ''' Аналог index.html в ServerRoot для начальной страницы
+    Returns:
+        [str] -- содержимое HTML-файла, заданного в глобальной
+            переменной ROOT_INDEX
+    '''
+    with open(ROOT_INDEX, 'r', encoding='utf-8') as f_:
+        return f_.read()
+
 ''' =====----- MAIN -----===== '''
 
 #####=====----- THE END -----=====#########################################
