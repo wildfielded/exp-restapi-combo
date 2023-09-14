c4aa4aa4180b2d758d373111d1b0f211140ff36a
Ded Wildfield <wildfielded@wildfielded.site>
2023-09-14 22:43:35 +0800

    Workday save2 14-09-2023

--- main_flask.py
+++ main_flask.py
@@ -5,6 +5,9 @@ import api_srv as api_
 
 ''' =====----- Global variables -----====='''
 
+# Корневой index.html
+ROOT_INDEX = 'adds_srv/index.html'
+
 
 ''' =====----- Server resources -----===== '''
 
