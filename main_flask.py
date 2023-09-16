56d06905e1222caa8c30349b0e9235af46faeac0
Ded Wildfield <wildfielded@wildfielded.site>
2023-09-16 18:22:56 +0800

    Workday save2 16-09-2023

--- main_flask.py
+++ main_flask.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 
+from flask import Flask
+
 import api_srv as api_
 
 
 ''' =====----- Global variables -----====='''
 
+app = Flask(__name__)
 # Корневой index.html
 ROOT_INDEX = 'adds_srv/index.html'
 
 
 ''' =====----- Server resources -----===== '''
 
+@app.route('/')
+def server_root() -> str:
+    ''' Аналог index.html в ServerRoot для начальной страницы
+    Returns:
+        [str] -- содержимое HTML-файла, заданного в глобальной
+            переменной ROOT_INDEX
+    '''
+    with open(ROOT_INDEX, 'r', encoding='utf-8') as f_:
+        return f_.read()
+
 
 ''' =====----- MAIN -----===== '''
 
 if __name__ == '__main__':
-    pass
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        debug=True
+       )
 
 #####=====----- THE END -----=====#########################################
