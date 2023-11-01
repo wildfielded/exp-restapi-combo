fa231c8181853ec5bd8d3577bd283ace93994653
Ded Wildfield <wildfielded@wildfielded.site>
2023-11-01 18:40:43 +0800

    Workday save1 01-11-2023

---
+++ main_fastapi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#####=====----- TEMPORAL for WinDev -----=====#####
+import sys
+sys.path.append('VENV\\Lib\\site-packages')
+###################################################
+
+from fastapi import FastAPI, responses
+
+#####=====----- THE END -----=====#########################################
