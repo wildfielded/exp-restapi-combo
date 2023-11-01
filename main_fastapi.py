3c3f1b966a4faeafd41d3ead7e54a0c79858de70
Ded Wildfield <wildfielded@wildfielded.site>
2023-11-01 19:00:46 +0800

    Workday save2 01-11-2023

--- main_fastapi.py
+++ main_fastapi.py
@@ -6,5 +6,31 @@ sys.path.append('VENV\\Lib\\site-packages')
 ###################################################
 
 from fastapi import FastAPI, responses
+import uvicorn
+
+
+''' =====----- Global variables -----====='''
+
+# Корневой index.html
+ROOT_INDEX = 'adds_srv/index.html'
+srv = FastAPI()
+
+
+''' =====----- Endpoints -----===== '''
+
+srv.get('/')
+async def server_root() -> str:
+    ''' Аналог index.html в ServerRoot для начальной страницы FastAPI
+    Rturns:
+        [str] -- содержимое HTML-файла, заданного в глобальной
+            переменной ROOT_INDEX
+    '''
+    return str(responses.FileResponse(ROOT_INDEX))
+    
+
+''' =====----- MAIN -----===== '''
+
+if __name__ == '__main__':
+    uvicorn.run('main_fastapi:srv', port=8080, reload=True)
 
 #####=====----- THE END -----=====#########################################
